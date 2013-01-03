27bc92b75c0d15e7d468cd11ea5ea1f2b6a54599
Andrea Zaupa <andrea.zaupa@develon.com>
2013-01-03 16:44:52 +0100

    merge

--- app/assets/javascripts/rails_admin/custom/rails_admin_globalize.js
+++ app/assets/javascripts/rails_admin/custom/rails_admin_globalize.js
@@ -7,7 +7,7 @@ $(function() {
 
 function updateQueryStringParameter(uri, key, value) {
   var re = new RegExp("([?|&])" + key + "=.*?(&|$)", "i");
-  separator = uri.indexOf('?') !== -1 ? "&" : "?";
+  var separator = uri.indexOf('?') !== -1 ? "&" : "?";
   if (uri.match(re)) {
     return uri.replace(re, '$1' + key + "=" + value + '$2');
   }
